7be362f8167ee400ff73488b27b6bd5922cd2522
Ahmed Vally <ahmedvally711@gmail.com>
2024-08-15 11:20:51 +0200

    added the dependencies and tried to fix error

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import ApiBlog from "../components/apiBlog";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <ApiBlog />
+    </>
+  );
+}
+
+export default App;
